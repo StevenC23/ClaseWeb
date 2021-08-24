7aaa4d896f1b2afebf5a3a500e29d19f7570b728
scarvajal0723 <scarvajal@vortexbird.com>
2021-08-24 16:04:13 -0500

    clase web

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ window.onscroll = () => {
   const fakeH = document.querySelector('#fakeHeader');
 
   if (window.innerWidth > 375) {
-    if (window.pageYOffset >= 150) {
+    if (window.pageYOffset >= 100) {
       fakeH.style.top = '0px';
       fakeH.style.opacity = '1';
     } else {
@@ -10,4 +10,23 @@ window.onscroll = () => {
       fakeH.style.opacity = '0';
     }
   }
+
 };
+
+let ubicacionPrincipal = window.pageYOffset;
+window.onscroll = function (){
+  let desplazamiento = window.pageYOffset;
+  console.log(window.pageXOffset);
+
+  if(ubicacionPrincipal >= desplazamiento){
+    // console.log(1);
+    document.getElementById('searchBar').style.top = '0px';
+    document.getElementById('footer').style.bottom = '-100px';
+  }else{
+    // console.log(2);
+    document.getElementById('searchBar').style.top = '-100px';
+    document.getElementById('footer').style.bottom = '0px';
+  }
+
+  ubicacionPrincipal = desplazamiento;
+}
